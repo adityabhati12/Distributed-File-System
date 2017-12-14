560d765e02f65b65c8116c70cbfda9a3cb8204f5
adityawebdevelopment <aditya.bhati05@gmail.com>
2017-12-14 20:23:40 +0000

    adding comments

--- authentication.py
+++ authentication.py
@@ -19,20 +19,20 @@ connection = MongoClient(connect_str)
 req_database = connection.filesystem
 req_database.filesystem_users.drop()
 req_database.filesystem_servers.drop()
-req_database.filesystem_directories.drop()
-req_database.filesystem_files.drop()
+req_database.directories.drop()
+req_database.files.drop()
 req_database.transactions.drop()
 
 hash_key.update("localhost" + ":" + "9001")
-req_database.servers.insert(
+req_database.filesystem_servers.insert(
     {"identifier": hash_key.hexdigest(), "host": "localhost", "port": "9001", "master_server": True, "in_use": False})
 
 hash_key.update("localhost" + ":" + "9002")
-req_database.servers.insert(
+req_database.filesystem_servers.insert(
     {"identifier": hash_key.hexdigest(), "host": "localhost", "port": "9002", "master_server": False, "in_use": False})
 
 hash_key.update("localhost" + ":" + "9002")
-req_database.servers.insert(
+req_database.filesystem_servers.insert(
     {"identifier": hash_key.hexdigest(), "host": "localhost", "port": "9003", "master_server": False, "in_use": False})
 
 application = Flask(__name__)
@@ -40,6 +40,9 @@ application = Flask(__name__)
 app = Flask(__name__)
 AUTH_KEY = "qwerty123456789asdfghjkladitya12"
 
+"""
+This functions creates the user and stores user details in the database
+"""
 
 @app.route('/user_creation', methods=['POST'])
 def user_creation():
@@ -50,7 +53,7 @@ def user_creation():
     u_id = r_data.get('u_id')
     u_password = r_data.get('u_password')
 
-    pass_encrypt = base64.b64encode(AES.new(p_key, AES.MODE_ECB).encrypt(u_password))
+    pass_encrypt = base64.b64encode(AES.new(p_key, AES.MODE_ECB).encrypt(u_password))#encoding the password
 
 
     period_id = "weewifu12uweed219i332jdnw4rdfweq"
@@ -58,11 +61,12 @@ def user_creation():
     req_database.filesystem_users.insert(
         {"u_id": u_id, "period_id": period_id, "p_key": p_key,
          "u_password": pass_encrypt}
-    )
+    )#adding data to the database
 
 
     return jsonify({"Msg": "Generated user",
-                    "User id": u_id})
+                    "User id": u_id})#returns a json serialisable type
+
 
 
 @app.route('/user_login', methods=['POST'])
@@ -76,7 +80,7 @@ def u_login():
     pass_encrypt = u_info['u_password']
     p_key = u_info['p_key']
 
-    decrypted_u_password = AES.new(p_key, AES.MODE_ECB).decrypt(base64.b64decode(pass_encrypt)).strip()
+    decrypted_u_password = AES.new(p_key, AES.MODE_ECB).decrypt(base64.b64decode(pass_encrypt)).strip()#decrypt user password
 
     if decrypted_u_password == u_password:
         period_id = ''.join(
